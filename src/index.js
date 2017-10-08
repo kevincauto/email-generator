290d6ba0757e7a13d41bb790c497b5bd5d83a292
Kevin Cauto <kevinmcauto@gmail.com>
2017-10-07 23:50:29 -0400

    The filtering has been updated.  The state now will hold separate objects for each template.

--- src/index.js
+++ src/index.js
@@ -1,123 +1,227 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {id_reader} from './email_templates/id-reader';
+import { id_reader } from './email_templates/id_reader';
 
 class Container extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        info: {
-            month: 'October',
-            volume: '10'
-        }
-        };
-        this.handleTextChange = this.handleTextChange.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected_template: 'info',
+      id_reader: {},
+      info: {}
+    };
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleTemplateChange = this.handleTemplateChange.bind(this);
+  }
 
-    handleTextChange(value, e) {
-        let info = {...this.state.info};
-        info[e.target.name] = value.trim();
-        this.setState({info});
-        console.log(this.state);
-    }
+  componentDidMount() {
+    //preload the selected templates object with default values?
+  }
 
-    render(){
-        return(
-            <div id="container">
-                <Form info={this.state.info} onTextChange={this.handleTextChange}/>
-                <TextResults info={this.state.info}/>
-            </div>
-        )
+  handleTextChange(value, e) {
+    let copyOfState = { ...this.state };
+    //within the state object, select the property that matches the selected template, then assign or reasign the propery that matches the name assigned to the textbox.  Be sure that the name attribute on the textbox is what you want this propery to be.
+    console.log(copyOfState);
+    copyOfState[this.state.selected_template][e.target.name] = value.trim();
+    console.log(copyOfState);
+    this.setState({ copyOfState });
+  }
 
-    }
+  handleTemplateChange(value) {
+    //change the selected template.  If coded correctly the change should cascade down and adjust everything.
+    let copyOfState = { ...this.state };
+    copyOfState.selected_template = value;
+    this.setState({ copyOfState });
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div id="container">
+        <Form
+          selected_template={this.state.selected_template}
+          info={this.state[this.state.selected_template]}
+          onTextChange={this.handleTextChange}
+          onTemplateChange={value => this.handleTemplateChange(value)}
+        />
+        <TextResults info={this.state.info} />
+      </div>
+    );
+  }
 }
 
 class Form extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            template: 'id-reader'
-        }
-        this.handleTemplateChange = this.handleTemplateChange.bind(this);
-        this.handleTextChange = this.handleTextChange.bind(this);
-    }
-    handleTextChange(e) {
-        this.props.onTextChange(e.target.value, e);
-    }
-    handleTemplateChange(event) {
-        this.setState({template: event.target.value});
-    }
-    
-    render(){
-    
-        let displayForm = <IDReaderForm />;
-        if(this.state.template === 'id-reader'){displayForm = <IDReaderForm />}
-        if(this.state.template === 'id-thematic'){displayForm = <IDThematicForm />}
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      template: 'id_reader'
+    };
+    this.handleTemplateChange = this.handleTemplateChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
 
-        return(
-            <div id="main-form">
-                <select value={this.state.value} onChange={this.handleTemplateChange}>
-                    <option value="id-reader">ID Reader</option>
-                    <option value="id-thematic">ID Thematic</option>
-                    <option value="idt-reader">IDT Reader</option>
-                    <option value="idt-thematic">IDT Thematic</option>
-                    <option value="cced-reader">CCED Reader</option>
-                </select>
+  handleTextChange(value, e) {
+    this.props.onTextChange(value, e);
+  }
 
-                {displayForm}
+  handleTemplateChange(value) {
+    this.props.onTemplateChange(value);
+  }
 
-                <div className="label">
-                    {/* The name attribute should match the state.info property */}
-                    Month: &nbsp;<input type="text" name="month" value={this.props.info.month} onChange={this.handleTextChange} />
-                </div>
-                    <br />
-                <div className="label">
-                    Volume: &nbsp;<input type="text" name="volume" value={this.props.info.volume} onChange={this.handleTextChange} />
-                </div>
-            </div> 
-        )
+  render() {
+    let displayForm = (
+      <IDReaderForm
+        selected_template={this.props.selected_template}
+        info={this.props[this.props.selected_template]}
+        onTextChange={this.handleTextChange}
+      />
+    );
+    if (this.state.template === 'id_reader') {
+      displayForm = (
+        <IDReaderForm
+          selected_template={this.props.selected_template}
+          info={this.props[this.props.selected_template]}
+          onTextChange={() => this.handleTextChange('id_reader')}
+        />
+      );
+    }
+    if (this.state.template === 'id_thematic') {
+      displayForm = (
+        <IDThematicForm
+          selected_template={this.props.selected_template}
+          info={this.props[this.props.selected_template]}
+          onTextChange={this.handleTextChange}
+        />
+      );
     }
 
+    return (
+      <div id="main-form">
+        <select value={this.state.value} onChange={this.handleTemplateChange}>
+          <option value="id_reader">ID Reader</option>
+          <option value="id_thematic">ID Thematic</option>
+          <option value="idt_reader">IDT Reader</option>
+          <option value="idt_thematic">IDT Thematic</option>
+          <option value="cced_reader">CCED Reader</option>
+        </select>
+        <h2>Complete the information below.</h2>
+        {displayForm}
+      </div>
+    );
+  }
 }
 
 class IDReaderForm extends React.Component {
-    render() {
-        return(
-            <div>Hello ID Reader!</div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(e) {
+    this.props.onTextChange(e.target.value, e);
+  }
+  render() {
+    return (
+      <div>
+        <h3> ID Reader</h3>
+        <div className="label">
+          {/* The name attribute should match the state.info property */}
+          Month: &nbsp;<input
+            type="text"
+            name="month"
+            value={this.props[this.props.selected_template].month}
+            onChange={this.handleTextChange}
+          />
+        </div>
+        <div className="label">
+          Volume: &nbsp;<input
+            type="text"
+            name="volume"
+            value={this.props[this.props.selected_template].volume}
+            onChange={this.handleTextChange}
+          />
+        </div>
+        <div className="label">
+          Year: &nbsp;
+          <select
+            value={this.props[this.props.selected_template].year}
+            name="year"
+            onChange={this.handleTextChange}
+          >
+            <option value="2017">2017</option>
+            <option value="2018">2018</option>
+            <option value="2019">2019</option>
+          </select>
+        </div>
+      </div>
+    );
+  }
 }
 
 class IDThematicForm extends React.Component {
-    render() {
-        return(
-            <div>Hello ID Thematic!</div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(e) {
+    this.props.onTextChange(e.target.value, e);
+  }
+  render() {
+    return (
+      <div>
+        <h3> ID Thematic</h3>
+        <div className="label">
+          {/* The name attribute should match the state.info property */}
+          Month: &nbsp;<input
+            type="text"
+            name="month"
+            value={this.props.info.month}
+            onChange={this.handleTextChange}
+          />
+        </div>
+        <div className="label">
+          Volume: &nbsp;<input
+            type="text"
+            name="volume"
+            value={this.props.info.volume}
+            onChange={this.handleTextChange}
+          />
+        </div>
+        <div className="label">
+          Year: &nbsp;<input
+            type="text"
+            name="year"
+            value={this.props.info.year}
+            onChange={this.handleTextChange}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 class TextResults extends React.Component {
-    render(){
-        const {month, volume} = this.props.info;
-        
-        const text =
-            `
+  render() {
+    const { month, volume, year } = this.props.info;
+
+    const text = `
                 ${id_reader.doctype}
                 <div>Hello ${month} World</div>
                 <div> This is the volume: ${volume} </div>
+                <div>${year}</div>
+                ${id_reader.head}
+                ${id_reader.bottom}
+
             `;
-       
-        return(
-            <div id="text-results" >
-                <textarea value={text} readOnly={true} />
-            </div>
-        )
-    }
+
+    return (
+      <div id="text-results">
+        <textarea value={text} readOnly={true} />
+      </div>
+    );
+  }
 }
 
-
 ReactDOM.render(<Container />, document.getElementById('root'));
-
-
