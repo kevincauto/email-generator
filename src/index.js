e458faf00f11ae5d408b115e6d83e0e6749068b4
Kevin Cauto <kevinmcauto@gmail.com>
2017-10-11 17:10:29 -0400

    quick update, a few comments.  pushing so I can work at home on this.

--- src/index.js
+++ src/index.js
@@ -71,12 +71,7 @@ class Form extends React.Component {
   }
 
   render() {
-    let displayForm = (
-        <CCEDLiveWebinar
-            info={this.props.info}
-            onTextChange={this.handleTextChange}
-        />
-    );
+    let displayForm;
 
 
     if (this.props.info.selected_template === 'cced_live_webinar') {
@@ -144,7 +139,7 @@ class TextResults extends React.Component {
                 </table>
             </td>
         </tr>`
-
+    //commented out by default
     let tag = `
     <!--	<tr>
         <td colspan="2">
@@ -159,6 +154,7 @@ class TextResults extends React.Component {
         </td>
       </tr>-->
       `
+      //if, however, the tagline is filled out...
       if(tagline){
         tag = `
         <tr>
@@ -258,10 +254,10 @@ class TextResults extends React.Component {
               <table cellpadding="0" cellspacing="0" border="0" align="left" bgcolor="#FFFFFF"  >
             <tr>
                 <td width="74">
-                    <a href="http://forms.coronapro.com/3MvJGQmB&returnredir=2017-03-30_pds" style="text-decoration:none;" target="_blank"><img src="http://forms.coronapro.com/images/tv_with_interference_full.gif" alt="Video Test" width="74" height="86" border="0" /></a>
+                    <a href="${tvLink}" style="text-decoration:none;" target="_blank"><img src="http://forms.coronapro.com/images/tv_with_interference_full.gif" alt="Video Test" width="74" height="86" border="0" /></a>
                 </td>
                 <td valign="center" align="left" width="328">
-                    <p style="font-weight: bold; font-size: 9pt; color:#D97B03; line-height: 13pt; margin: 0; padding: 11px 0 0 11px; font-family:Arial, Helvetica, sans-serif"><a href="http://forms.coronapro.com/3MvJGQmB&returnredir=2017-03-30_pds" style="color:#c2904a; text-decoration:none;" target="_blank">Be sure to test your setup here <em><span style="font-size:10pt;'">BEFORE</span></em> the Webinar to ensure everything is working properly!</a></p>
+                    <p style="font-weight: bold; font-size: 9pt; color:#D97B03; line-height: 13pt; margin: 0; padding: 11px 0 0 11px; font-family:Arial, Helvetica, sans-serif"><a href="${tvLink}" style="color:#c2904a; text-decoration:none;" target="_blank">Be sure to test your setup here <em><span style="font-size:10pt;'">BEFORE</span></em> the Webinar to ensure everything is working properly!</a></p>
                 </td>
             </tr>
         </table>
@@ -317,28 +313,120 @@ class TextResults extends React.Component {
   
     </body></html>
   `
-
-
-
-
-
-
-
-
-
-    var html = first + tag + main + lo + tv + theRest;
-
-    
-
+    let html = first + tag + main + lo + tv + theRest;
 
     return (
       <div id="text-results">
-       
         <div className="content" dangerouslySetInnerHTML={{__html: html}}></div>
-        
       </div>
     );
   }
 }
 
 ReactDOM.render(<Container />, document.getElementById('root'));
+
+cced_on_demand = `
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Now On Demand</title>
+<style type="text/css">
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+</style>
+</head>
+
+<body bgcolor="#efefef">
+	<table width="600" cellpadding="0" cellspacing="0" border="0" align="center" style="font-family:'Helvetica','Arial',sans-serif;background-color:#ffffff">
+	<tbody>
+    <tr>
+		<td colspan="2" style="font-size:9px; line-height:20px; text-transform:uppercase; text-align:center; color:#333333; background-color:#efefef;">
+		Cannot view this email? 
+			<a href="http://aegispublications.com/news/cced/2017/09/Instrumentarium-5-On-Demand.html" target="_blank" style="text-decoration:none; color:#c2904a;">
+				Click here to view the HTML version.
+			</a>
+		</td>
+	</tr>
+    <tr>
+		<td colspan="2">
+			<table width="100%" cellpadding="0" cellspacing="0" border="0">
+				<tbody><tr>
+				  <td height="39" colspan="2" align="center" valign="top" id="cover2"><a href="https://cced.cdeworld.com/" target="_blank"><img src="http://www.aegispublications.com/news/cdeworld/2016/03/cced-header.jpg" width="659" height="75" hspace="0" vspace="0" border="0" align="left"></a></td>
+			  </tr>
+              <tr>
+			</tr></tbody></table>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2">
+			<table width="590" cellpadding="0" cellspacing="0" border="0" style="border-bottom:1px solid #e4e4e4; margin:0 32px 20px 32px">
+				<tbody><tr>
+					<td style="padding:21px 0 21px 0; font-family:'Times New Roman',serif;font-size:28px; font-style:italic; color:#c2904a;">Free On-Demand CE Webinar</td>
+				</tr>
+			</tbody></table>
+		</td>
+	</tr>
+	
+	<tr>
+		<td colspan="2" valign="top" style="padding:12px 32px 24px 32px;font-size:13px;color:#424242;line-height:16px">
+			<table cellpadding="0" cellspacing="0" border="0" align="right" style="padding:0 0 30px 0">
+			<tbody><tr>
+				<td bgcolor="#ffffff" style="border-collapse:collapse;padding:0 14px 10px 0"></td>
+			  <td align="right" valign="top">
+				<a href="https://cdeworld.com/webinars/20778-Getting_started_with_CBCT_Imaging_for_Implant_Planning_and_Surgical_Guides" target="_blank"><img src="https://www.dentalaegis.com/media/63260/" alt="" width="299"></a><br>
+				</td>
+			</tr>
+		</tbody></table>	
+        	<div style="font-size:18px; line-height:20px; font-weight:bold; width:40%;"><a href="https://cdeworld.com/webinars/20778-Getting_started_with_CBCT_Imaging_for_Implant_Planning_and_Surgical_Guides" target="_blank" style="color:#c2904a; text-decoration:none;">Getting started with CBCT Imaging for Implant Planning and Surgical Guides</a></div>
+        
+        	
+            <a href="https://cdeworld.com/webinars/20778-Getting_started_with_CBCT_Imaging_for_Implant_Planning_and_Surgical_Guides" target="_blank" style="text-transform:uppercase; font-size:16px; background:#c2904a; padding:10px 11px;color:#ffffff; border-radius:10px; font-weight:bold; display:block; width:193px; text-align:center; margin:14px 0 14px 0; text-decoration:none;">VIEW THE WEBINAR</a><br />
+			<strong>Presenter:</strong> Douglas Chenin, DDS | <strong>Provider:</strong> AEGIS Publications, LLC<br>					
+			<em><strong>Commercial Supporter:</strong> Kavo Kerr</em><br>
+			<strong>CDE Credits:</strong> 1 Self Study | <strong>Cost:</strong> $0.00 | August 2017 - August 31st, 2020<br><br />
+			<strong>Description</strong><br>
+			<div style="margin:5px 0 0 0">This webinar will introduce CBCT and 3D imaging in general practice with a focus on its value for implant planning and surgical guides. </div>		
+			<br>
+			<strong>Learning Objectives</strong>
+			<ul style="margin:5px 0 0 0; padding:0 0 0 1.3em;">
+				<li>Conceptualize the rationale for 3D imaging and how 3D imaging enhances diagnostics beyond 2D imaging for implant planning</li>
+				<li>Understand how to asses and visualize patient CBCT data properly in 3D and 2D cross-sections for clinical applications</li>
+				<li>Demonstrate the foundational 3D imaging principles by exploring sample cases within 3D imaging software</li>
+			</ul>
+            <strong><br>
+            Disclosures</strong><br>
+			<div style="margin:5px 0 0 0;">Dr. Chenin has received an honorarium for his preparation and presentation of this program.</div>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+			<img src="http://aegispublications.com/news/id/webinars/webinar-footer.jpg" width="600" height="85" style="border-top:1px solid #e4e4e4;" />
+		</td>
+	</tr>
+	<tr>
+	  <td colspan="2" align="center" style="font-size:10px; font-family:Arial, Helvetica, sans-serif; color:#424242; padding:11px 0 11px 0; border-top:solid 1px #efefef; background-color:#efefef;">
+      	<a href="mailto:?subject=On Demand Webinar!&amp;body=I thought you might be interested in this: http://aegispublications.com/news/cced/2017/09/Instrumentarium-5-On-Demand.html" target="_blank" style="text-decoration:none; color:#c2904a;">
+				Forward to a Colleague
+			</a>
+			&nbsp;&nbsp;|&nbsp;&nbsp; 
+			<a href="http://www.dentalaegis.com/privacy-policy/" target="_blank" style="text-decoration:none; color:#c2904a;">
+				Privacy Policy
+			</a>
+				<br />
+			<em>Compendium</em>  |  104 Pheasant Run, Suite 105  |  Newtown, PA 18940
+			  <br />
+		    
+		%%PLUGIN_UNSUBSCRIBE: 2144642-UNSUBSCRIBE%%
+</div> 
+      </td>
+	</tr>	
+  </tbody>
+</table>
+</body>
+</html>
+`
