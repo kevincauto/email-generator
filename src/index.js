1366bd0a08cacdec0711ee06244a0ef4ce15009c
Kevin Cauto <kevincauto@Brandon-Underwoods-iMac.local>
2017-10-11 09:54:45 -0400

    IDReader and IDThematic were removed.  This will keep the code decluttered as I work on the webinar specific emails.

--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ class Container extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected_template: 'id_reader',
-      id_reader: {},
-      id_thematic: {},
-      cced_webinar_recruitment: {}
+      selected_template: 'cced_live_webinar',
+      cced_live_webinar: {}
     };
     this.handleTextChange = this.handleTextChange.bind(this);
     this.handleTemplateChange = this.handleTemplateChange.bind(this);
@@ -77,29 +75,14 @@ class Form extends React.Component {
 
   render() {
     let displayForm = (
-      <IDReaderForm
-        info={this.props.info}
-        onTextChange={this.handleTextChange}
-      />
+        <CCEDLiveWebinar
+            info={this.props.info}
+            onTextChange={this.handleTextChange}
+        />
     );
-    if (this.props.info.selected_template === 'id_reader') {
-      displayForm = (
-        <IDReaderForm
-          info={this.props.info}
-          onTextChange={this.handleTextChange}
-        />
-      );
-    }
-    if (this.props.info.selected_template === 'id_thematic') {
-      displayForm = (
-        <IDThematicForm
-          info={this.props.info}
-          onTextChange={this.handleTextChange}
-        />
-      );
-    }
 
-    if (this.props.info.selected_template === 'cced_webinar_recruitment') {
+
+    if (this.props.info.selected_template === 'cced_live_webinar') {
         displayForm = (
           <CCEDLiveWebinar
             info={this.props.info}
@@ -114,10 +97,9 @@ class Form extends React.Component {
           value={this.props.info.selected_template}
           onChange={this.handleTemplateChange}
         >
-          <option value="id_reader">ID Reader</option>
-          <option value="id_thematic">ID Thematic</option>
-          <option value="cced_webinar_recruitment">CCED Webinar Recruitment</option>
-
+          <option value="cced_live_webinar">CCED Live Webinar</option>
+          {/* <option value="cced_live_webinar_tomorrow">CCED Live Webinar Tomorrow</option>
+          <option value="cced_live_webinar_2hours">CCED Live Webinar in Two Hours</option> */}
         </select>
         <h2>Complete the information below.</h2>
         {displayForm}
@@ -126,95 +108,7 @@ class Form extends React.Component {
   }
 }
 
-class IDReaderForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleTextChange = this.handleTextChange.bind(this);
-  }
 
-  handleTextChange(e) {
-    this.props.onTextChange(e.target.value, e.target.name);
-  }
-  render() {
-    return (
-      <div>
-        <h3> ID Reader</h3>
-        <div className="label">
-          {/* The name attribute should match the state.info property */}
-          Month: &nbsp;<input
-            type="text"
-            name="month"
-            value={this.props.info[this.props.info.selected_template].month}
-            onChange={this.handleTextChange}
-          />
-        </div>
-        <div className="label">
-          Volume: &nbsp;<input
-            type="text"
-            name="volume"
-            value={this.props.info[this.props.info.selected_template].volume}
-            onChange={this.handleTextChange}
-          />
-        </div>
-        <div className="label">
-          Year: &nbsp;
-          <select
-            value={this.props.info[this.props.info.selected_template].years}
-            name="year"
-            onChange={this.handleTextChange}
-          >
-            <option value="2017">2017</option>
-            <option value="2018">2018</option>
-            <option value="2019">2019</option>
-          </select>
-        </div>
-      </div>
-    );
-  }
-}
-
-class IDThematicForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleTextChange = this.handleTextChange.bind(this);
-  }
-
-  handleTextChange(e) {
-    this.props.onTextChange(e.target.value, e.target.name);
-  }
-  render() {
-    return (
-      <div>
-        <h3> ID Thematic</h3>
-        <div className="label">
-          {/* The name attribute should match the state.info property */}
-          Month: &nbsp;<input
-            type="text"
-            name="month"
-            value={this.props.info[this.props.info.selected_template].month}
-            onChange={this.handleTextChange}
-          />
-        </div>
-        <div className="label">
-          Volume: &nbsp;<input
-            type="text"
-            name="volume"
-            value={this.props.info[this.props.info.selected_template].volume}
-            onChange={this.handleTextChange}
-          />
-        </div>
-        <div className="label">
-          Year: &nbsp;<input
-            type="text"
-            name="year"
-            value={this.props.info[this.props.info.selected_template].year}
-            onChange={this.handleTextChange}
-          />
-        </div>
-      </div>
-    );
-  }
-}
 
 class TextResults extends React.Component {
   render() {
