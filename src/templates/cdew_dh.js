e05fd4a60a18c6e8231476456b16599d970feb47
Kevin Cauto <kevinmcauto@gmail.com>
2018-02-01 17:00:21 -0500

    Fixed the CDEWorld so that the footers work correctly.

--- src/templates/cdew_dh.js
+++ src/templates/cdew_dh.js
@@ -299,7 +299,7 @@ export function beginning(fields) {
   return `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
   <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-  <title>New CE Courses For Dental Assistants</title>
+  <title>New CE Courses For Dental Hygienists</title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
